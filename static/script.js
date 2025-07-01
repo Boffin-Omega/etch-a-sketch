f4367300516f75d596efcb61746d5a9b009b880e
Boffin-Omega <c.kgagangowda78@gmail.com>
2025-07-01 19:49:51 +0530

    Implemented reset grid and change grid size buttons

--- static/script.js
+++ static/script.js
@@ -1,20 +1,40 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    let n = 4;
-    let container = document.querySelector(".container");
-    //to create 16*16 grid
-    let pixelSize = container.offsetWidth / n;
-    
-    for(let i = 0; i < n; i++){
-        let row = document.createElement("div");
-        row.className = "row";
-        for(let j = 0; j < n; j++){
-            let pixel = document.createElement("div");
-            pixel.className = "pixel";
-            pixel.style.width = `${pixelSize}px`;
-            pixel.style.height = `${pixelSize}px`;
+let n = 16;
 
-            row.appendChild(pixel);
+function draw_grid(n){
+    let container = document.querySelector(".container");
+        let pixelSize = container.offsetWidth / n;
+
+        //to create nxn grud
+        for(let i = 0; i < n; i++){
+            let row = document.createElement("div");
+            row.className = "row";
+            for(let j = 0; j < n; j++){
+                let pixel = document.createElement("div");
+                pixel.className = "pixel";
+                pixel.style.width = `${pixelSize}px`;
+                pixel.style.height = `${pixelSize}px`;
+
+                row.appendChild(pixel);
+            }
+            container.appendChild(row);
         }
-        container.appendChild(row);
-    }
+}
+document.addEventListener("DOMContentLoaded",()=>{
+    draw_grid(n)    
+    document.querySelector(".nav").addEventListener("click",(e)=>{
+        if(e.target.id == "change"){
+            //to change n
+            n = prompt("Enter new grid size"); 
+            if(n <1 || n >100 || !Number.isInteger(n))alert("Invalid grid size! Must be between 1 and 100");
+            else{
+                document.querySelector(".container").innerHTML="";
+                draw_grid(n);
+            }
+        }
+        else{
+            //sort of eraser, to reset all the 'drawing'
+            document.querySelector(".container").innerHTML="";
+            draw_grid(n);
+        }
+    })
 })
